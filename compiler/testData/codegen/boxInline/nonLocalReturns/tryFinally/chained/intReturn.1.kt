2f67a1db441a0ea2cabebbaff9574394b5a80161
Michael Bogdanov <mikhael.bogdanov@jetbrains.com>
2015-06-23 11:08:39 +0300

    tests

--- compiler/testData/codegen/boxInline/nonLocalReturns/tryFinally/chained/intReturn.1.kt
+++ compiler/testData/codegen/boxInline/nonLocalReturns/tryFinally/chained/intReturn.1.kt
@@ -17,85 +17,6 @@ fun test0(h: Holder, throwException: Boolean): Int {
     return -1;
 }
 
-//fun test1(h: Holder): Int {
-//    val localResult = doCall2 (
-//            {
-//                h.value += "OK_NONLOCAL"
-//                return 1
-//            },
-//            {
-//                h.value += ", OK_EXCEPTION"
-//                "OK_EXCEPTION"
-//            },
-//            {
-//                h.value += ", OK_FINALLY"
-//                "OK_FINALLY"
-//            }, "FAIL")
-//
-//    return -1;
-//}
-//
-//fun test2(h: Holder): String {
-//    val localResult = doCall (
-//            {
-//                h.value += "OK_NONLOCAL"
-//                return "OK_NONLOCAL"
-//            },
-//            {
-//                h.value += ", OK_EXCEPTION"
-//                2
-//            },
-//            {
-//                h.value += ", OK_FINALLY"
-//                3
-//            })
-//
-//    return "" + localResult;
-//}
-//
-//fun test3(h: Holder): String {
-//    val localResult = doCall (
-//            {
-//                h.value += "OK_NONLOCAL"
-//                if (true) {
-//                    throw java.lang.RuntimeException()
-//                }
-//                return "OK_NONLOCAL"
-//            },
-//            {
-//                h.value += ", OK_EXCEPTION"
-//                return "OK_EXCEPTION"
-//            },
-//            {
-//                h.value += ", OK_FINALLY"
-//                3
-//            })
-//
-//    return "" + localResult;
-//}
-//
-//fun test4(h: Holder): String {
-//    val localResult = doCall (
-//            {
-//                h.value += "OK_NONLOCAL"
-//                if (true) {
-//                    throw java.lang.RuntimeException()
-//                }
-//                h.value += "fail"
-//                return "OK_NONLOCAL"
-//            },
-//            {
-//                h.value += ", OK_EXCEPTION"
-//                return "OK_EXCEPTION"
-//            },
-//            {
-//                h.value += ", OK_FINALLY"
-//                return "OK_FINALLY"
-//            })
-//
-//    return "" + localResult;
-//}
-
 fun box(): String {
     var h = Holder()
     val test0 = test0(h, true)
@@ -104,22 +25,6 @@ fun box(): String {
     h = Holder()
     val test1 = test0(h, false)
     if (test1 != 1 || h.value != "OK_NONLOCAL, OK_FINALLY") return "test1: ${test1}, holder: ${h.value}"
-
-//    h = Holder()
-//    val test1 = test1(h)
-//    if (test1 != 1 || h.value != "OK_NONLOCAL, OK_FINALLY") return "test1: ${test1}, holder: ${h.value}"
-//
-//    h = Holder()
-//    val test2 = test2(h)
-//    if (test2 != "OK_NONLOCAL" || h.value != "OK_NONLOCAL, OK_FINALLY") return "test2: ${test2}, holder: ${h.value}"
-//
-//    h = Holder()
-//    val test3 = test3(h)
-//    if (test3 != "OK_EXCEPTION" || h.value != "OK_NONLOCAL, OK_EXCEPTION, OK_FINALLY") return "test3: ${test3}, holder: ${h.value}"
-//
-//    h = Holder()
-//    val test4 = test4(h)
-//    if (test4 != "OK_FINALLY" || h.value != "OK_NONLOCAL, OK_EXCEPTION, OK_FINALLY") return "test4: ${test4}, holder: ${h.value}"
 
     return "OK"
 }
--- compiler/testData/codegen/boxInline/nonLocalReturns/tryFinally/chained/intReturnComplex3.1.kt
+++ compiler/testData/codegen/boxInline/nonLocalReturns/tryFinally/chained/intReturnComplex3.1.kt
@@ -17,57 +17,16 @@ fun test0(h: Holder, throwException: Boolean): Int {
     return -1;
 }
 
-//fun test3(h: Holder): String {
-//    val localResult = doCall (
-//            {
-//                h.value += "OK_NONLOCAL"
-//                if (true) {
-//                    throw java.lang.RuntimeException()
-//                }
-//                return "OK_NONLOCAL"
-//            },
-//            {
-//                h.value += ", OK_EXCEPTION"
-//                return "OK_EXCEPTION"
-//            },
-//            {
-//                h.value += ", OK_FINALLY"
-//                3
-//            })
-//
-//    return "" + localResult;
-//}
-//
-//fun test4(h: Holder): String {
-//    val localResult = doCall (
-//            {
-//                h.value += "OK_NONLOCAL"
-//                if (true) {
-//                    throw java.lang.RuntimeException()
-//                }
-//                h.value += "fail"
-//                return "OK_NONLOCAL"
-//            },
-//            {
-//                h.value += ", OK_EXCEPTION"
-//                return "OK_EXCEPTION"
-//            },
-//            {
-//                h.value += ", OK_FINALLY"
-//                return "OK_FINALLY"
-//            })
-//
-//    return "" + localResult;
-//}
-
 fun box(): String {
     var h = Holder()
     val test0 = test0(h, true)
-    if (test0 != -1 || h.value != "OK_NONLOCAL, OK_EXCEPTION, OK_FINALLY, DO_CALL_2_FINALLY, DO_CALL_2_1_FINALLY, DO_CALL_2_2_FINALLY") return "test0: ${test0}, holder: ${h.value}"
+    if (test0 != -1 || h.value != "OK_NONLOCAL, OK_EXCEPTION, OK_FINALLY, DO_CALL_2_FINALLY, DO_CALL_2_1_FINALLY, DO_CALL_2_2_FINALLY")
+        return "test0: ${test0}, holder: ${h.value}"
 
     h = Holder()
     val test1 = test0(h, false)
-    if (test1 != 1 || h.value != "OK_NONLOCAL, OK_FINALLY, DO_CALL_2_FINALLY, DO_CALL_2_1_FINALLY, DO_CALL_2_2_FINALLY") return "test1: ${test1}, holder: ${h.value}"
+    if (test1 != 1 || h.value != "OK_NONLOCAL, OK_FINALLY, DO_CALL_2_FINALLY, DO_CALL_2_1_FINALLY, DO_CALL_2_2_FINALLY")
+        return "test1: ${test1}, holder: ${h.value}"
 
     return "OK"
 }
