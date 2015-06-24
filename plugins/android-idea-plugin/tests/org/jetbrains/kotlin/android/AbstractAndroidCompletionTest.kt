098ede166261e2a4277b6a8fa439dceaf83e4439
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-06-24 18:56:03 +0300

    ++ Minor: fix deprecations in Kotlin Android Extensions code

--- plugins/android-idea-plugin/tests/org/jetbrains/kotlin/android/AbstractAndroidCompletionTest.kt
+++ plugins/android-idea-plugin/tests/org/jetbrains/kotlin/android/AbstractAndroidCompletionTest.kt
@@ -34,10 +34,10 @@ public abstract class AbstractAndroidCompletionTest : KotlinAndroidTestCase() {
         codeCompletionOldValue = settings.AUTOCOMPLETE_ON_CODE_COMPLETION
         smartTypeCompletionOldValue = settings.AUTOCOMPLETE_ON_SMART_TYPE_COMPLETION
 
+        @suppress("NON_EXHAUSTIVE_WHEN")
         when (completionType()) {
             CompletionType.SMART -> settings.AUTOCOMPLETE_ON_SMART_TYPE_COMPLETION = false
             CompletionType.BASIC -> settings.AUTOCOMPLETE_ON_CODE_COMPLETION = false
-            else -> {}
         }
     }
 
