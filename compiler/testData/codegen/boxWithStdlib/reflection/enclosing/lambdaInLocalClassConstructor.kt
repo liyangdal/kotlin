ab971a98ca36466a20b3743dd89aa0e4d127b433
Michael Bogdanov <mikhael.bogdanov@jetbrains.com>
2015-06-25 18:34:05 +0300

    test fix

--- compiler/testData/codegen/boxWithStdlib/reflection/enclosing/lambdaInLocalClassConstructor.kt
+++ compiler/testData/codegen/boxWithStdlib/reflection/enclosing/lambdaInLocalClassConstructor.kt
@@ -12,10 +12,10 @@ fun box(): String {
 
     val javaClass = l.a.javaClass
     val enclosingMethod = javaClass.getEnclosingConstructor()!!.getName()
-    if (!enclosingMethod.startsWith("_DefaultPackage\$") || !enclosingMethod.endsWith("\$box\$L")) return "ctor: $enclosingMethod"
+    if (!enclosingMethod.startsWith("LambdaInLocalClassConstructor\$") || !enclosingMethod.endsWith("\$box\$L")) return "ctor: $enclosingMethod"
 
     val enclosingClass = javaClass.getEnclosingClass()!!.getName()
-    if (!enclosingClass.startsWith("_DefaultPackage\$") || !enclosingClass.endsWith("\$box\$L")) return "enclosing class: $enclosingClass"
+    if (!enclosingClass.startsWith("LambdaInLocalClassConstructor\$") || !enclosingClass.endsWith("\$box\$L")) return "enclosing class: $enclosingClass"
 
     if (enclosingMethod != enclosingClass) return "$enclosingClass != $enclosingMethod"
 
